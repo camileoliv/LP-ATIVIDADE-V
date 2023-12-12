039ec0d7a9151a55700278a405d394fbb9dad4d3
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-12-12 14:36:49 -0300

    Add files via upload

---
+++ Atividade V.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+struct Atividade V
+{
+    /* data */
+};
+
+
+
+int main () {
+    printf("Sistema de gerenciamento.\n");
+    printf("1- Entrada de produtos. \n");
+    printf("2- Saída de produtos. \n");
+    printf("3- Inventário. \n");
+    printf("4- Funcionários presentes no site. \n");
+
+
+    return 0;
+}
