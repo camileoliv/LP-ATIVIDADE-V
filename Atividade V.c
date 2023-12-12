be4bf5afc871eeff32a7e14ead682c8e0ba75f25
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-12-12 16:02:38 -0300

    Update Atividade V.c

--- Atividade V.c
+++ Atividade V.c
@@ -4,20 +4,137 @@
 #include <string.h>
 #include <time.h>
 
-struct Atividade V
+#define TAM 100
+
+struct produto
 {
-    /* data */
+    char nome[200];
+    float preco;
+    int estoque;
 };
 
+struct dados
+{
+    char nome[200];
+    float preco;
+    int estoqueA;
+};
 
+void automatizacao(int estoqueA, int vendasQuan)
+{
+    int estoqueAtualizado;
+    estoqueAtualizado = estoqueA - vendasQuan;
+    printf("Em estoque: %d \n", estoqueAtualizado);
+}
 
-int main () {
-    printf("Sistema de gerenciamento.\n");
-    printf("1- Entrada de produtos. \n");
-    printf("2- Saída de produtos. \n");
-    printf("3- Inventário. \n");
-    printf("4- Funcionários presentes no site. \n");
+int main()
+{
+    // Declarando Variáveis
+    struct produto entrada[TAM];
+    struct produto saida[TAM];
+    struct dados atual[TAM];
 
+    int code, quantidade, contador, i, j, vendasDaily, vendasQuan;
+    char diaReferencia[10];
+    float ganhos;
+
+    setlocale(LC_ALL, "portuguese");
+
+    do
+    {
+
+        printf("Sistema de gerenciamento.\n");
+        printf("1- Entrada de produtos. \n");
+        printf("2- Saída de produtos. \n");
+        printf("3- Inventário. \n");
+        printf("4- Funcionários presentes no site. \n");
+        printf("Resposta: ");
+        scanf("%i", &code);
+        system("cls || clear");
+
+        switch (code)
+        {
+        case 1:
+            printf("Digite a quantidade de produtos: ");
+            scanf("%i", &quantidade);
+
+            for (i = 0; i < quantidade; i++)
+            {
+                fflush(stdin);
+                printf("Digite o %iº produto: ", i + 1);
+                gets(entrada[i].nome);
+                strcpy(atual[i].nome, entrada[i].nome);
+                contador++;
+
+                printf("Digite o preço do %iº produto: ", i + 1);
+                scanf("%f", &entrada[i].preco);
+                atual[i].preco = entrada[i].preco;
+
+                printf("Digite a quantidade do %iº produto: ", i + 1);
+                scanf("%i", &entrada[i].estoque);
+                atual[i].estoqueA = entrada[i].estoque;
+                system("cls || clear");
+            }
+            break;
+
+        case 2:
+            fflush(stdin);
+            printf("Total de produtos vendidos: ");
+            scanf("%d", &vendasDaily);
+
+            fflush(stdin);
+
+            printf("Dia de referência (DD/MM/AAAA): ");
+            gets(diaReferencia);
+
+            printf("Ganhos totais: ");
+            scanf("%f", &ganhos);
+
+            for (j = 0; j < vendasDaily; j++)
+            {
+                fflush(stdin);
+
+                printf("Qual o nome do %iº produto? ", j + 1);
+                gets(saida[j].nome);
+
+                fflush(stdin);
+
+                if (strcmp(saida[j].nome, entrada[j].nome) == 0)
+                {
+                    printf("Digite a quantidade vendida: ");
+                    scanf("%i", &vendasQuan);
+                }
+                else
+                {
+                    printf("Produto inválido.");
+                }
+                break;
+            }
+
+            break;
+
+        case 3:
+            fflush(stdin);
+            for (i = 0; i < contador; i++)
+            {
+                printf("Produto: %s\n", atual[i].nome);
+                printf("Preço: %.2f\n", atual[i].preco);
+                automatizacao(atual[i].estoqueA, vendasQuan);
+            }
+
+            break;
+
+        case 4:
+            fflush(stdin);
+
+            /*/printf("");/*/
+
+            break;
+
+        default:
+            break;
+        }
+    } while (code != 4);
 
     return 0;
 }
